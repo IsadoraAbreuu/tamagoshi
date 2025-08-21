a95d3c79530787a5ddfd3f702d42cf18a428ee90
Isadora Abreu <isaabreucorrea@gmail.com>
2025-08-21 11:25:42 -0300

    Update tamagoshi.py

--- tamagoshi.py
+++ tamagoshi.py
@@ -1,4 +1,6 @@
+#classe Tamagoshi - classe pai
 class Tamagoshi:
+    #atributos do Tamagoshi normal - padrão
     def __init__(self, nome):
         self.nome = nome
         self.fome = 0
@@ -6,6 +8,7 @@ class Tamagoshi:
         self.idade = 0
         self.tedio = 0
 
+    #métodos essenciais para todos os Tamagoshis
     def alimentar(self, quantidade):
         if (quantidade >= 0) and (quantidade <= 100):
             self.fome -= self.fome * (quantidade /100)
@@ -31,21 +34,48 @@ class Tamagoshi:
             self.saude = 0   
             print("Seu bichinho morreu T_T")
 
+    #função que chamas os métodos essenciais
     def tempoPassando(self):
         self.vida()
         self.idade += 0.2
         self.tedio += 2.5
         self.fome += 5
 
+
+#classe para Tamagoshi Gato
 class TamagoshiGato(Tamagoshi):
+    #atributos para Tamagoshi Gato
     def __init__(self, nome, energia, banho):
-        super().__init__(self, nome)
+        super().__init__(self, nome) #puxa atributos da classe pai
         self.energia = energia
         self.banho = banho
 
+    #m
+
+
+#classe para Tamagoshi Cachorro
 class TamagoshiCachorro(Tamagoshi):
-    def __init__(self, nome, passear, brincar, treinar):
-        super().__init__(self, nome):
+    #atributos para Tamagoshi Cachorro
+    def __init__(self, nome, passear, treinar):
+        super().__init__(self, nome) #puxa atributos da classe pai
         self.passear = passear
-        self.brincar = brincar
         self.treinar = treinar
+
+
+#classe para Tamagoshi Passaro
+class TamagoshiPassaro(Tamagoshi):
+    #atributos para Tamagoshi Passaro
+    def __init__(self, nome, assobio, plumagem):
+        super().__init__(nome) #puxa atributos da classe pai
+        self.assobio = assobio 
+        self.plumagem = plumagem
+    
+    def assobiar(self):
+        print(f"O pássaro {self.nome} está assobiando! {self.assobio}")
+    
+
+
+
+passaro1 = TamagoshiPassaro("luli", "piu piu piuuuu", "roxa")
+
+passaro1.assobiar()
